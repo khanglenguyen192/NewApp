3a576dff9596b3792bbd9cefab8fd7ef4e9c61ea
FPTSHOP <FPTSHOP@LE-NGUYEN-KHANG>
2021-06-17 09:15:04 +0700

    Add project files.

---
+++ NewApp/DetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NewApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailPage : ContentPage
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
---
+++ NewApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NewApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonEnter_OnClicked(object sender, EventArgs e)
+        {
+            txtName.Text = "Name: " + txtNumber.Text;
+            // imgImage.Background = "green";
+            // imgImage.Source = ImageSource.FromFile("Resources/drawable/book_tltd.jpg");
+
+        }
+
+        private void ButtonPage2_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DetailPage());
+        }
+
+
+    }
+}
